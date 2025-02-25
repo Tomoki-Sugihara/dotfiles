bdad40aeffacf1b8f6e18cabe073e81a6f090812
t-sugihara <sugihara.10969@gmail.com>
2025-02-26 08:44:19 +0900

    Fix Cursor setup script: correct symlink and extension installation command

--- cursor/cursor-setup.sh
+++ cursor/cursor-setup.sh
@@ -3,11 +3,11 @@ CURSOR_SETTING_DIR=~/Library/Application\ Support/Cursor/User
 
 
 ln -s ~/dotfiles/cursor/settings.json ${CURSOR_SETTING_DIR}/settings.json
-ln -s ~/dotfiles/cursor/keybindings.json ${CURSOR_SETTING_DIR}/keybindings.jsons
+ln -s ~/dotfiles/cursor/keybindings.json ${CURSOR_SETTING_DIR}/keybindings.json
 
 # TODO: Signature error出る
 # install extension
 cat ~/dotfiles/cursor/extensions | while read line
 do
-  cursor --force --install-extension $line
+  code --force --install-extension $line
 done
