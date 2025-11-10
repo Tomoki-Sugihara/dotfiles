dd5009ab042c45738d51382adb9d24d319f47779
Tomoki Sugihara <sugihara.10969@gmail.com>
2025-11-10 09:54:06 +0900

    feat: add brave-browser to Brewfile, new keybinding for composerMode, and set cursor as git commit editor

--- git/git-setup.sh
+++ git/git-setup.sh
@@ -1,2 +1,5 @@
 git config --global user.name "t-sugihara"
 git config --global user.email "sugihara.10969@gmail.com"
+
+# cursorでコミットメッセージを編集する
+git config --global core.editor "cursor --wait --reuse-window"
